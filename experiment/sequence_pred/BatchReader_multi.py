a29c8e38f125df1cdef7b6f9bc43e95ef61ef4c4
gepu0221 <cauc_gp@163.com>
2018-07-18 16:16:06 +0800

    BatchReader_multi modfied on 0718

--- experiment/sequence_pred/BatchReader_multi.py
+++ experiment/sequence_pred/BatchReader_multi.py
@@ -1,7 +1,8 @@
-#Mulitthreading batchreader for sequence FCN first modifed on 20180712.
+#Multithreading batchreader for sequence FCN first modifed on 20180712.
 import numpy as np
+import tensorflow as tf
 import scipy.misc as misc
-import cv2
+import os
 import time
 
 
@@ -16,6 +17,7 @@ class BatchDatset:
     files = []
     seq_images = []   #sequences[0..4] in combine with seq[7]
     cur_images = [] #current image only
+    filenames = []
 
     annotations = []
     image_options = {}
@@ -31,7 +33,7 @@ class BatchDatset:
         print('image_options: ', image_options)
         self.files = records_list
         self.image_options = image_options
-        self._read_images()
+        #self._read_images()
 
     def _read_cur(self):
         self.cur_images = np.array([self.transform_current(filename['current']) for filename in self.files])
@@ -47,15 +49,10 @@ class BatchDatset:
     def fuse_seq(self, cur_filename, seq_set_filename):
         #
         self.count += 1
-
         current_frame = self.transform_rgb(cur_filename)
-        seq_frame = np.array([np.expand_dims(self.transform_gray(seq_filename), axis=2) for seq_filename in seq_set_filename])
-        #print(current_frame.shape)
-        #print(seq_frame.shape)
-        if seq_frame.shape[0] == cfgs.seq_num:
-            frame = current_frame
-            for i in range(cfgs.seq_num):
-                frame = np.concatenate((frame, seq_frame[i]), axis=2)
+        frame = current_frame
+        for i in range(0,cfgs.seq_num):
+            frame = tf.concat((frame, self.transform_gray(seq_set_filename[i])), 2)
         if self.count % 1000 == 0:
             print('--image%d--' % (self.count))
         return frame
@@ -68,8 +65,7 @@ class BatchDatset:
 
         if self.image_options.get("resize", False) and self.image_options["resize"]:
             resize_size = int(self.image_options["resize_size"])
-            resize_image = misc.imresize(image,
-                                         [resize_size, resize_size], interp='nearest')
+            resize_image = misc.imresize(image,[resize_size, resize_size], interp='nearest')
         else:
             resize_image = image
 
@@ -77,64 +73,56 @@ class BatchDatset:
 
 
     def transform_rgb(self, filename):
-        image = cv2.imread(filename)
-        if len(image.shape) < 3:
-            image = np.array([image for i in range(3)])
+        image = tf.read_file(filename)
+        image = tf.image.decode_image(image, channels=3)
             
         if self.image_options.get("resize", False) and self.image_options["resize"]:
             resize_size = int(self.image_options["resize_size"])
-            resize_image = cv2.resize(image, (resize_size, resize_size), interpolation=cv2.INTER_AREA)
+            resize_image = tf.image.resize_bilinear([image], size=[resize_size, resize_size])[0]
         else:
             resize_image = image
 
-        return np.array(resize_image)
+        return resize_image
     
     def transform_gray(self, filename):
-        image = cv2.imread(filename)
-        if len(image.shape) == 3:
-            gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        else:
-            raise Exception('Picture read failed!!!')
+        image = tf.read_file(filename)
+        image = tf.image.decode_image(image, channels=1)
             
         if self.image_options.get("resize", False) and self.image_options["resize"]:
             resize_size = int(self.image_options["resize_size"])
-            resize_image = cv2.resize(gray_image, (resize_size, resize_size), interpolation=cv2.INTER_AREA)
+            resize_image = tf.image.resize_bilinear([image], size=[resize_size, resize_size])[0]
         else:
             resize_image = gray_image
 
-        return np.array(resize_image)
+        return resize_image
 
     def _read_images_list(self):
         
+        #seq, cur, anno, filename
         self.seq_images = [item['seq'] for item in self.files]
         self.cur_images = [item['current'] for item in self.files]
-        if self.image_option.get('annotation', False) and self.image_options['annotation']:
+        if self.image_options.get('annotation', False) and self.image_options['annotation']:
+            print('anno exists')
             self.annotations = [item['annotation'] for item in self.files]
-        
-        self.flienames = [item['filename'] for item in self.files]
+       
+        self.filenames =  [item['filename'] for item in self.files]
+
 
         data = tf.data.Dataset.from_tensor_slices((self.seq_images, self.cur_images, self.annotations, self.filenames))
 
         return data
 
-    def _parse_record(self, cur_filename, seq_filename_set, anno_filename=None, filename):
+    def _parse_record(self, seq_filename_set, cur_filename, anno_filename, filename):
         
         #fuse sequence
         fuse_im = self.fuse_seq(cur_filename, seq_filename_set)
-        
-        #get annotation
-        if self.image_options.get('annotation', False) and self.image_options['annotation']:
-            anno_im = self.transform_gray(anno_filename)
-        else:
-            anno_im = None
-        
-        #get current frame
-        if self.image_options.get('visualize', False) and self.image_options['visualize']:
-            cur_im = self._read_cur(cur_filename)
-        else:
-            cur_im = None
 
-        return fuse_im, anno_im, cur_im, filename
+        print('fuse_im', tf.shape(fuse_im))
+        #get annotation
+        anno_im = tf.cast(self.transform_gray(anno_filename), tf.int32)
+        print('anno_im', tf.shape(anno_im))
+
+        return fuse_im, anno_im, filename
 
         
 
@@ -145,16 +133,19 @@ class BatchDatset:
 
             n_cpu_cores = os.cpu_count()
 
-            data_list = data.map(lambda fuse_im, anno_im, cur_im, filename: _parse_record(fuse_im, anno_im, cur_im, filename), num_parallel_calls=n_cpu_cores)
+            data_list = data.map(self._parse_record, num_parallel_calls=n_cpu_cores)
             data_batch = data_list.batch(batch_size)
+
+            data_batch = data_batch.repeat()
 
             data_batch = data_batch.prefetch(5)
 
             iterator = tf.data.Iterator.from_structure(data_batch.output_types, data_batch.output_shapes) 
             data_init = iterator.make_initializer(data_batch, name='data_init')
 
-            fuse_im_batch, anno_im_batch, cur_im_batch, filename_batch = iterator.get_next()
+            fuse_im_batch, anno_im_batch, filename_batch = iterator.get_next()
+            print('shape of fuse_batch', tf.shape(fuse_im_batch)[1])
             im_size = int(self.image_options['resize_size'])
-            fuse_im_batch.set([ batch_size, im_size, im_size, cfgs.seq_num+3])
+            fuse_im_batch.set_shape([ batch_size, im_size, im_size, cfgs.seq_num+3])
         
-        return fuse_im_batch, anno_im_batch, cur_im_batch, filename_batch, data_init
+        return fuse_im_batch, anno_im_batch, filename_batch, data_init
